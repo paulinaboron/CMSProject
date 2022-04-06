8c54fb99ee99856a133218306cbfc1434fe85980
Paulina Boroń <pboron@dc514i.zsl>
2022-04-06 12:29:31 +0200

    sdad

--- frontend/svelte/src/App.svelte
+++ frontend/svelte/src/App.svelte
@@ -2,6 +2,37 @@
   export let name;
 </script>
 
+
+<main>
+
+  <!-- Nawigacja -->
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul
+          class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+          style="--bs-scroll-height: 100px"
+        >
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/">Link</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <button class="btn btn-sm btn-outline-primary">Register</button>
+          <button class="btn btn-sm btn-outline-success" id="login-btn">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  <h1>Hello {name}!</h1>
+  <p>Main Svelte-Page</p>
+</main>
+
 <style>
   main {
     text-align: center;
@@ -22,8 +53,3 @@
     }
   }
 </style>
-
-<main>
-  <h1>Hello {name}!</h1>
-  <p>Main Svelte-Page</p>
-</main>
