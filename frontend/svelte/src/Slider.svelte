ea0d7db2a12bcda9b7b422d32a3197a5fdea7d6c
Paulina Boroń <pboron@dc514i.zsl>
2022-04-06 12:57:01 +0200

    sadsad

--- frontend/svelte/src/Slider.svelte
+++ frontend/svelte/src/Slider.svelte
@@ -4,5 +4,87 @@
     console.log(id)
 </script>
 
-<h1>Slajder</h1>
-<h2>Id: {id}</h2>
+<h2>Id slidera: {id}</h2>
+
+<!-- Slider -->
+<div
+id="carouselExampleDark"
+class="carousel carousel-dark slide"
+data-bs-ride="carousel"
+>
+<div class="carousel-indicators">
+  <button
+    type="button"
+    data-bs-target="#carouselExampleDark"
+    data-bs-slide-to="0"
+    class="active"
+    aria-current="true"
+    aria-label="Slide 1"
+  ></button>
+  <button
+    type="button"
+    data-bs-target="#carouselExampleDark"
+    data-bs-slide-to="1"
+    aria-label="Slide 2"
+  ></button>
+  <button
+    type="button"
+    data-bs-target="#carouselExampleDark"
+    data-bs-slide-to="2"
+    aria-label="Slide 3"
+  ></button>
+</div>
+<div class="carousel-inner">
+  <div class="carousel-item active" data-bs-interval="9000">
+    <img
+      src="../../uploads/slider/gray1.webp"
+      class="d-block w-100 slider-picture"
+      alt="..."
+    />
+    <div class="carousel-caption d-none d-md-block">
+      <h5>First slide label</h5>
+      <p>Some representative placeholder content for the first slide.</p>
+    </div>
+  </div>
+  <div class="carousel-item" data-bs-interval="9000">
+    <img
+      src="../../uploads/slider/gray2.jpg"
+      class="d-block w-100 slider-picture"
+      alt="..."
+    />
+    <div class="carousel-caption d-none d-md-block">
+      <h5>Second slide label</h5>
+      <p>Some representative placeholder content for the second slide.</p>
+    </div>
+  </div>
+  <div class="carousel-item" data-bs-interval="9000">
+    <img
+      src="../../uploads/slider/gray3.jpg"
+      class="d-block w-100 slider-picture"
+      alt="..."
+    />
+    <div class="carousel-caption d-none d-md-block">
+      <h5>Third slide label</h5>
+      <p>Some representative placeholder content for the third slide.</p>
+    </div>
+  </div>
+</div>
+<button
+  class="carousel-control-prev"
+  type="button"
+  data-bs-target="#carouselExampleDark"
+  data-bs-slide="prev"
+>
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Previous</span>
+</button>
+<button
+  class="carousel-control-next"
+  type="button"
+  data-bs-target="#carouselExampleDark"
+  data-bs-slide="next"
+>
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Next</span>
+</button>
+</div>
