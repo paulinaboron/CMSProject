a5b4e22571351d15a3017658f824f56bbe0e6d0d
Paulina Boroń <pboron@dc514i.zsl>
2022-04-07 11:26:13 +0200

    componenty svelte

--- frontend/svelte/src/App.svelte
+++ frontend/svelte/src/App.svelte
@@ -2,6 +2,11 @@
   export let name;
   import Navigation from "./Navigation.svelte"
   import Slider from "./Slider.svelte"
+  import Footer from "./Footer.svelte"
+  import Card from "./Card.svelte"
+  import Featurette from "./Featurette.svelte"
+
+  console.log(name);
 </script>
 
 
@@ -9,28 +14,15 @@
 
   <Navigation/>
   <Slider id=1/>
+  <Card/>
+  <Featurette/>
 
-  <h1>Hello {name}!</h1>
-  <p>Main Svelte-Page</p>
+  
+  <Footer/>
+
+
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
 </style>
