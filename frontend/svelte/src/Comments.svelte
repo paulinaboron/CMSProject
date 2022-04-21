1832eab7e7d0d3dc2ae8f5219a8cbaae3cc56d55
Paulina Boroń <pboron@dc514i.zsl>
2022-04-21 12:44:18 +0200

    nav

--- frontend/svelte/src/Comments.svelte
+++ frontend/svelte/src/Comments.svelte
@@ -41,6 +41,22 @@
                 {/each}
                 
                 {/await}
+
+
+                <form>
+                	<h3 class="pull-left">Dodaj komentarz</h3>
+                	<button type="submit" class="btn btn-normal pull-right">OK</button>
+                    <fieldset>
+                        <div class="row">
+                            <div class="col-sm-3 col-lg-2 hidden-xs">
+                            	<img class="img-responsive" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
+                            </div>
+                            <div class="form-group col-xs-12 col-sm-9 col-lg-10">
+                                <textarea class="form-control" id="message" placeholder="Your message" required=""></textarea>
+                            </div>
+                        </div>  	
+                    </fieldset>
+                </form>
                 
             </div>
         </div>
--- frontend/svelte/src/Navigation.svelte
+++ frontend/svelte/src/Navigation.svelte
@@ -1,19 +1,15 @@
 <script>
     async function getLinks() {
-        let response = await fetch(
-            `/getLinks?component=header`,
-            { method: "post" }
-        );
+        let response = await fetch(`/getLinks?component=header`, {
+            method: "post",
+        });
         let responseJson = await response.json();
         console.log("response nav: ", responseJson);
         return responseJson;
     }
 
     async function getLoggedUser() {
-        let response = await fetch(
-            `/getLoggedUserData`,
-            { method: "post" }
-        );
+        let response = await fetch(`/getLoggedUserData`, { method: "post" });
         let responseJson = await response.json();
         console.log("response logUser: ", responseJson);
         return responseJson;
@@ -24,128 +20,79 @@
 
     $: navData = getLinks();
     $: loggedUserData = getLoggedUser();
-
-    let windowWidth = window.innerWidth;
-
-    window.addEventListener("resize", function () {
-        windowWidth = window.innerWidth;
-    });
 </script>
 
 {#await navData}
     <h1>Oczekiwanie na dane nawigacji</h1>
 {:then navData}
-    {#if windowWidth > 990}
-        <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-                <div class="collapse navbar-collapse" id="navbarScroll">
-                    <ul
-                        class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
-                        style="--bs-scroll-height: 100px"
-                    >
-                        {#each navData as item}
-                            <li class="nav-item">
-                                <a class="nav-link" href={item.url}
-                                    >{item.text}</a
-                                >
-                            </li>
-                        {/each}
-                    </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon" />
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    {#each navData as item}
+                        <li class="nav-item">
+                            <a class="nav-link mt-1" href={item.url}
+                                >{item.text}</a
+                            >
+                        </li>
+                    {/each}
 
                     {#await loggedUserData}
                         <h1>Oczekiwanie na dane użytkownika</h1>
                     {:then loggedUserData}
                         {#if loggedUserData.error_message}
                             <form class="d-flex">
+                                <li class="nav-item nav-link">
                                 <a
                                     href="/register"
                                     class="btn btn-sm btn-outline-primary"
                                     >Register</a
                                 >
+                            </li>
+                            <li class="nav-item nav-link">
                                 <a
                                     href="/login"
                                     class="btn btn-sm btn-outline-success"
                                     id="login-btn"
                                 >
                                     Login
-                                </a>
+                                </a></li>
                             </form>
                         {:else}
-                            <span class="px-3">
+                        <li class="nav-item nav-link">
+                            <span class="mr-2 mt-1">
                                 Zalogowany jako: <strong
                                     >{loggedUserData.userName}</strong
                                 >
                             </span>
 
-                            <a href="/logoutUser">Wyloguj</a>
+                            <a href="/logoutUser">Wyloguj</a></li>
                         {/if}
                     {/await}
-                </div>
-            </div>
-        </nav>
-    {:else}
-        <!--Navbar-->
-        <nav class="navbar navbar-light light-blue lighten-4">
-            <!-- Navbar brand -->
-            <a class="navbar-brand" href="#">Navbar</a>
-
-            <!-- Collapse button -->
-            <button
-                class="navbar-toggler toggler-example"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent1"
-                aria-controls="navbarSupportedContent1"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-                ><span class="dark-blue-text"
-                    ><i class="fas fa-bars fa-1x" /></span
-                ></button
-            >
-
-            <!-- Collapsible content -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent1">
-                <!-- Links -->
-                <ul class="navbar-nav mr-auto">
-                    {#each navData as item}
-                        <li class="nav-item">
-                            <a class="nav-link" href={item.url}>{item.text}</a>
-                        </li>
-                    {/each}
                 </ul>
-
-                {#await loggedUserData}
-                    <h1>Oczekiwanie na dane użytkownika</h1>
-                {:then loggedUserData}
-                    {#if loggedUserData.error_message}
-                        <form class="d-flex">
-                            <a
-                                href="/register"
-                                class="btn btn-sm btn-outline-primary"
-                                >Register</a
-                            >
-                            <a
-                                href="/login"
-                                class="btn btn-sm btn-outline-success"
-                                id="login-btn"
-                            >
-                                Login
-                            </a>
-                        </form>
-                    {:else}
-                        <span class="px-3">
-                            Zalogowany jako: <strong
-                                >{loggedUserData.userName}</strong
-                            >
-                        </span>
-
-                        <a href="/logoutUser">Wyloguj</a>
-                    {/if}
-                {/await}
-                <!-- Links -->
             </div>
-            <!-- Collapsible content -->
-        </nav>
-        <!--/.Navbar-->
-    {/if}
+
+            <form class="d-flex">
+                <input
+                    class="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                />
+                <button class="btn btn-outline-success" type="submit">OK</button
+                >
+            </form>
+        </div>
+    </nav>
 {/await}
