6558462caf6e4ff5098cb8e5255b2d303f4f48e8
Michał Kurbiel <Kurbiel Micha@PPINF.LOCAL>
2022-04-25 12:20:04 +0200

    wyszukiwarka

---
+++ frontend/svelte/src/Results.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Navigation from "./Navigation.svelte";
+    import Footer from "./Footer.svelte";
+    import Card from "./Card.svelte"
+
+    let ids = new URLSearchParams(window.location.search).get("ids").split(',');
+  </script>
+  
+  <main>
+    <Navigation />
+    <div class="container">
+
+    </div>
+    <h1>Wyniki wyszukiwania</h1>
+
+    {#each ids as id}
+      <Card id={id}/>
+    {/each}
+
+    <Footer sticky={true}/>
+  </main>
+  
