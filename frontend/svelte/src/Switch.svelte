f3f191b03c9521814b71deca1676310d142292eb
mkurbiel42 <michal.kurbiel42@gmail.com>
2022-04-28 16:09:46 +0200

    kolory z bazy danych + przycisk

--- frontend/svelte/src/Switch.svelte
+++ frontend/svelte/src/Switch.svelte
@@ -1,17 +1,17 @@
 <script>
-	const convertToHSL = (color = "rgb(24, 25, 26)") => {
+	const rgbStringToHsl = (color = "rgb(24, 25, 26)") => {
 		let colorsString = color.substring(4, color.length - 1);
-		let rgbColors = colorsString.split(", ").map((elem) => {return parseInt(elem)});
-		let rgbPrims = rgbColors.map((elem) => {return elem/255})
+		let rgbColors = colorsString.split(", ").map((elem) => {
+			return parseInt(elem);
+		});
+		let rgbPrims = rgbColors.map((elem) => {
+			return elem / 255;
+		});
 
-		console.log(rgbColors, rgbPrims)
+		let CMax = Math.max(rgbPrims[0], rgbPrims[1], rgbPrims[2]);
+		let CMin = Math.min(rgbPrims[0], rgbPrims[1], rgbPrims[2]);
 
-		let CMax = Math.max(rgbPrims[0], rgbPrims[1], rgbPrims[2])
-		let CMin = Math.min(rgbPrims[0], rgbPrims[1], rgbPrims[2])
-
-		let delta = CMax - CMin
-
-		console.log(CMax, CMin, delta)
+		let delta = CMax - CMin;
 
 		let hue;
 		let sat;
@@ -19,46 +19,94 @@
 
 		switch (true) {
 			case delta == 0:
-				hue = 0
+				hue = 0;
 				break;
 			case CMax == rgbPrims[0]:
-				hue = 60 * (((rgbPrims[1] - rgbPrims[2]) / delta) % 6)
+				hue = 60 * (((rgbPrims[1] - rgbPrims[2]) / delta) % 6);
 				break;
 
 			case CMax == rgbPrims[1]:
-				hue = 60 * (((rgbPrims[2] - rgbPrims[0]) / delta) + 2)
+				hue = 60 * ((rgbPrims[2] - rgbPrims[0]) / delta + 2);
 				break;
 
 			case CMax == rgbPrims[2]:
-				hue = 60 * (((rgbPrims[0] - rgbPrims[1]) / delta) + 4)
+				hue = 60 * ((rgbPrims[0] - rgbPrims[1]) / delta + 4);
 				break;
 			default:
 				break;
 		}
 
-		light = (CMax + CMin) / 2
+		light = (CMax + CMin) / 2;
 
-		if(delta == 0){
-			sat = 0
-		}else{
-			sat = delta / (1 - Math.abs((2 * light) - 1));
+		if (delta == 0) {
+			sat = 0;
+		} else {
+			sat = delta / (1 - Math.abs(2 * light - 1));
 		}
-
-
-		console.log("HSL", hue, sat * 100, light * 100)
 
 		return {
-			hue: hue,
+			hue: Math.round(hue),
 			saturation: sat.toFixed(2) * 100,
 			light: light.toFixed(2) * 100
-		}
-	}
+		};
+	};
 
-	const darken = ({hue, saturation, light}) => {
-		
-	}
+	const forDark = (color) => {
+		let { hue, saturation, light } = color;
+
+		return {
+			hue,
+			saturation: Math.round(saturation * 0.92),
+			light: Math.round(light * 0.85)
+		};
+	};
+
+	const forBorder = (color) => {
+		let { hue, saturation, light } = color;
+
+		return {
+			hue,
+			saturation: Math.round(saturation + (100 - saturation) * 0.15),
+			light: Math.round(light * 0.78)
+		};
+	};
+
+	const forShadow = (color) => {
+		let { hue, saturation, light } = color;
+
+		return {
+			hue,
+			saturation,
+			light: Math.round(light + (100 - light) * 0.15)
+		};
+	};
+
+	const hslToHslString = (color) => {
+		let { hue, saturation, light } = color;
+
+		return `hsl(${hue}, ${saturation}%, ${light}%)`;
+	};
+
+	const hslToHslaString = (color, alpha) => {
+		let { hue, saturation, light } = color;
+
+		return `hsla(${hue}, ${saturation}%, ${light}%, ${alpha})`;
+	};
+
 	const setRootStyle = (key, val) => {
 		document.documentElement.style.setProperty(key, val);
+	};
+
+	const darkColor = (color) => {
+		return hslToHslString(forDark(rgbStringToHsl(color)));
+	};
+
+	const borderColor = (color) => {
+		return hslToHslString(forBorder(rgbStringToHsl(color)));
+	};
+
+	const shadowColor = (color) => {
+		return hslToHslaString(forShadow(rgbStringToHsl(color)), 0.5);
 	};
 
 	const setColors = (darkMode) => {
@@ -68,12 +116,30 @@
 			setRootStyle("--font-color", "white");
 			setRootStyle("--icon-color", "white");
 			setRootStyle("--button-color", "rgb(90, 90, 90)");
+			setRootStyle("--button-color-dark", darkColor("rgb(90, 90, 90)"));
+			setRootStyle(
+				"--button-border-color-dark",
+				borderColor("rgb(90, 90, 90)")
+			);
+			setRootStyle(
+				"--button-shadow-color",
+				shadowColor("rgb(90, 90, 90)")
+			);
 		} else {
 			setRootStyle("--bg-color", colors.bg_color);
 			setRootStyle("--card-color", colors.bg_color);
 			setRootStyle("--font-color", colors.font_color);
 			setRootStyle("--icon-color", colors.icon_color);
 			setRootStyle("--button-color", colors.button_color);
+			setRootStyle("--button-color-dark", darkColor(colors.button_color));
+			setRootStyle(
+				"--button-border-color-dark",
+				darkColor(colors.button_color)
+			);
+			setRootStyle(
+				"--button-shadow-color",
+				shadowColor(colors.button_color)
+			);
 		}
 	};
 
@@ -84,7 +150,7 @@
 		darkMode = darkModeResp;
 		setColors(darkMode);
 		return darkModeResp;
-	}
+	};
 
 	const switchDarkMode = () => {
 		fetch("/switchDarkMode", { method: "POST" }).then(() => {
@@ -93,10 +159,10 @@
 	};
 
 	const getTemplateColors = async () => {
-		let response = await fetch("/getTemplateColors")
+		let response = await fetch("/getTemplateColors");
 		let responseJSON = await response.json();
-		return responseJSON
-	}
+		return responseJSON;
+	};
 
 	const init = async () => {
 		colors = await getTemplateColors();
@@ -105,10 +171,9 @@
 
 	let darkMode = false;
 	let darkModeFetch;
-	let colors = {}
+	let colors = {};
 
 	init();
-	convertToHSL();
 </script>
 
 {#await darkModeFetch then darkModeFetch}
@@ -121,8 +186,9 @@
 			on:change={switchDarkMode}
 			bind:checked={darkMode}
 		/>
-		<label class="form-check-label darkmode-text" for="flexSwitchCheckDefault"
-			>Dark</label
+		<label
+			class="form-check-label darkmode-text"
+			for="flexSwitchCheckDefault">Dark</label
 		>
 	</div>
 {/await}
