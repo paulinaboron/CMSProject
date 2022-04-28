cf808dc0fe4dd0d0b140b375bee19eea1420b5ad
Paulina Boroń <pboron@dc514i.zsl>
2022-04-28 12:03:35 +0200

    profil

--- frontend/svelte/src/Navigation.svelte
+++ frontend/svelte/src/Navigation.svelte
@@ -88,7 +88,7 @@
 								{/if}
 
 								<li class="nav-item">
-									<a class=" nav-link mt-1" href="/profil"
+									<a class=" nav-link mt-1" href="/profile"
 										>Zalogowany jako: <strong
 											>{loggedUserData.userName}
 										</strong></a
@@ -146,7 +146,7 @@
 					{/if}
 
 					<li class="nav-item">
-						<a class="nav-link" href="/profil">
+						<a class="nav-link" href="/profile">
 							Zalogowany jako: <strong
 								>{loggedUserData.userName}
 							</strong>
