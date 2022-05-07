e058b1d3f0f3f88dd858725e0dce478f94a4208d
paulinaboron <paulinaboron04@gmail.com>
2022-05-07 22:33:39 +0200

    tkinter

--- backend/admin.py
+++ backend/admin.py
@@ -23,14 +23,14 @@ def log_in():
         role = records[0][1]
 
         if passw == pass_entry.get() and role == 'admin':
-            users_window()
+            menu_window()
         else:
             txt = Label(root, text='Niepoprawne dane')
             txt.grid(row=3, column=1)
 
 
 def menu_window():
-    # root.destroy()
+    root.destroy()
 
     window = Tk()
     window.title('CMS')
@@ -38,32 +38,38 @@ def menu_window():
 
     btn_back = Button(window, text="Strona główna", command=lambda: click(main_page), width=30).grid(row=10, column=0)
 
-    main_page = Frame(window, bg='red')
+    main_page = Frame(window)
     users_page = Frame(window)
     menu_page = Frame(window)
     news_page = Frame(window)
-    footer_page = Frame(window)
     slider_page = Frame(window)
 
-    l1 = Label(users_page, text="Użytkownicy", font=('Helvetica', 14)).grid(row=0, column=0)
+    for frame in (main_page, users_page, menu_page, news_page, slider_page):
+        frame.grid(row=0, column=0, sticky='news')
+
+    click(main_page)
+
+    """Users Page"""
+    title_users = Label(users_page, text="Użytkownicy", font=('Helvetica', 14), pady=20, padx=50).grid(row=0, column=0,
+                                                                                                       sticky='e')
     users_data = select_from_db("Select * from users")
 
     tab = ["lp.", 'Login', "Hasło", "email", "rola"]
     for i in range(len(tab)):
         lab = Label(users_page, text=tab[i])
-        lab.grid(row=0, column=i, sticky=NSEW)
+        lab.grid(row=1, column=i, sticky=NSEW)
+
     for i in range(len(users_data)):
         for j in range(len(users_data[0])):
             e = Entry(users_page, relief=GROOVE)
-            e.grid(row=i+1, column=j, sticky=NSEW)
+            e.grid(row=i + 2, column=j, sticky=NSEW)
             e.insert(END, (users_data[i][j]))
 
-    l2 = Label(menu_page, text="mmmm").grid(row=1, column=0)
-    l3 = Label(news_page, text="nwewe").grid(row=1, column=0)
+    for i in range(len(users_data)):
+        b = Button(users_page, text="Edit", command=lambda temp=i: edit_users(temp, window), padx=15).grid(row=i + 2,
+                                                                                                           column=10)
 
-    for frame in (main_page, users_page, menu_page, news_page, footer_page, slider_page):
-        frame.grid(row=0, column=0, sticky='news')
-
+    """Main Page"""
     label = Label(main_page, text="Zarządzanie zawartością strony", font=('Helvetica', 14), pady=50).grid(row=0,
                                                                                                           column=0)
     btn_users = Button(main_page, text="Użytkownicy", command=lambda: click(users_page), pady=10, width=50).grid(row=1,
@@ -71,42 +77,100 @@ def menu_window():
     btn_menu = Button(main_page, text="Menu", command=lambda: click(menu_page), pady=10, width=50).grid(row=2, column=0)
     btn_news = Button(main_page, text="Artykuły", command=lambda: click(news_page), pady=10, width=50).grid(row=3,
                                                                                                             column=0)
-    btn_footer = Button(main_page, text="Stopka", command=lambda: click(footer_page), pady=10, width=50).grid(row=4,
-                                                                                                              column=0)
     btn_slider = Button(main_page, text="Slider", command=lambda: click(slider_page), pady=10, width=50).grid(row=5,
                                                                                                               column=0)
 
-    click(main_page)
+    """Slider Page"""
+    title_users = Label(slider_page, text="Slider", font=('Helvetica', 14), pady=20, padx=50).grid(row=0, column=0,
+                                                                                                   sticky='e')
+    slides_data = select_from_db("Select * from slides")
 
+    for i in range(len(slides_data)):
+        e1 = Entry(slider_page, relief=GROOVE)
+        e1.grid(row=i + 2, column=0, sticky=NSEW)
+        e1.insert(END, (slides_data[i][0]))
 
-def users_window():
-    root.destroy()
+        e2 = Entry(slider_page, relief=GROOVE)
+        e2.grid(row=i + 2, column=1, sticky=NSEW)
+        e2.insert(END, (slides_data[i][4]))
 
-    window = Tk()
-    window.title('CMS')
-    window.geometry("1000x600")
+        e3 = Entry(slider_page, relief=GROOVE, width=70)
+        e3.grid(row=i + 2, column=2, sticky=NSEW)
+        e3.insert(END, (slides_data[i][5]))
 
-    title = Label(window, text="Użytkownicy", font=('Helvetica', 14), pady=20, padx=50).grid(row=0, column=0, sticky='e')
-    users_data = select_from_db("Select * from users")
+        b_slider = Button(slider_page, text="Edit", command=lambda temp=i: edit_slider(temp, window), padx=15).grid(
+            row=i + 2,
+            column=10)
 
-    tab = ["lp.", 'Login', "Hasło", "email", "rola"]
-    for i in range(len(tab)):
-        lab = Label(window, text=tab[i])
-        lab.grid(row=1, column=i, sticky=NSEW)
+    """Menu Page"""
+    title_menu = Label(menu_page, text="Menu", font=('Helvetica', 14), pady=20, padx=50).grid(row=0, column=0,
+                                                                                                   sticky='e')
+    menu_data = select_from_db("Select * from nav_links")
 
-    for i in range(len(users_data)):
-        for j in range(len(users_data[0])):
-            e = Entry(window, relief=GROOVE)
-            e.grid(row=i+2, column=j, sticky=NSEW)
-            e.insert(END, (users_data[i][j]))
+    for i in range(len(menu_data)):
+        e1 = Entry(menu_page, relief=GROOVE)
+        e1.grid(row=i + 2, column=0, sticky=NSEW)
+        e1.insert(END, (menu_data[i][0]))
 
-    for i in range(len(users_data)):
-        b = Button(window, text="Edit", command=lambda temp=i: edit(temp, window), padx=15).grid(row=i+2, column=10)
-        print(i, "iiii")
+        e2 = Entry(menu_page, relief=GROOVE)
+        e2.grid(row=i + 2, column=1, sticky=NSEW)
+        e2.insert(END, (menu_data[i][2]))
+
+        b_menu = Button(menu_page, text="Edit", command=lambda temp=i: edit_menu(temp, window), padx=15).grid(
+            row=i + 2,
+            column=10)
+
+    b_add = Button(menu_page, text="Dodaj", command=lambda: add_link(window)).grid(row=1, column=12)
+
+    """News Page"""
+    title_news = Label(news_page, text="Artykuły", font=('Helvetica', 14), pady=20, padx=50).grid(row=0, column=0,
+                                                                                                   sticky='e')
+    news_data = select_from_db("Select * from articles")
+
+    for i in range(len(news_data)):
+        e1 = Entry(news_page, relief=GROOVE)
+        e1.grid(row=i + 2, column=0, sticky=NSEW)
+        e1.insert(END, (news_data[i][0]))
+
+        e2 = Entry(news_page, relief=GROOVE)
+        e2.grid(row=i + 2, column=1, sticky=NSEW)
+        e2.insert(END, (news_data[i][1]))
+
+        b_news = Button(news_page, text="Edit", command=lambda temp=i: edit_news(temp, window), padx=15).grid(
+            row=i + 2,
+            column=10)
 
 
 def click(frame):
     frame.tkraise()
+
+
+def add_link(w):
+    add_window = Toplevel(w)
+    add_window.title("Dodawanie linku")
+    add_window.geometry("600x400")
+
+    label = Label(add_window, text="Dodawanie linku").grid(row=0, column=1)
+
+    tab = ["Link", "Tekst", "Komponent", "Kolejność"]
+
+    e1 = Entry(add_window, width=70)
+    e1.grid(row=1, column=1, sticky=NSEW)
+
+    e2 = Entry(add_window, width=70)
+    e2.grid(row=2, column=1, sticky=NSEW)
+
+    e3 = Entry(add_window, width=70)
+    e3.grid(row=3, column=1, sticky=NSEW)
+
+    e4 = Entry(add_window, width=70)
+    e4.grid(row=4, column=1, sticky=NSEW)
+
+    for i in range(len(tab)):
+        l = Label(add_window, text=tab[i]).grid(row=i + 1, column=0)
+
+    btn = Button(add_window, text="Zapisz",
+                 command=lambda: save_link([e1.get(), e2.get(), e3.get(), e4.get(), add_window])).grid(row=5, column=1)
 
 
 def select_from_db(query):
@@ -119,14 +183,14 @@ def select_from_db(query):
     return records
 
 
-def edit(_id, w):
+def edit_users(_id, w):
     print(11, _id)
     edit_window = Toplevel(w)
     edit_window.title("Edytowanie")
     edit_window.geometry("600x400")
 
     label = Label(edit_window, text="Edycja danych użytkownika").grid(row=0, column=1)
-    records = select_from_db(f"Select * from users where id = '{_id+1}'")
+    records = select_from_db(f"Select * from users where id = '{_id + 1}'")
     data = records[0]
 
     tab = ["Login", "Hasło", "Rola"]
@@ -144,17 +208,146 @@ def edit(_id, w):
     e3.grid(row=3, column=1, sticky=NSEW)
 
     for i in range(len(tab)):
-        l = Label(edit_window, text=tab[i]).grid(row=i+1, column=0)
+        l = Label(edit_window, text=tab[i]).grid(row=i + 1, column=0)
 
-    btn = Button(edit_window, text="Zapisz zmiany", command=lambda: save([_id+1, e1.get(), e2.get(), e3.get()])).grid(row=4, column=1)
+    btn = Button(edit_window, text="Zapisz zmiany",
+                 command=lambda: save_users([_id + 1, e1.get(), e2.get(), e3.get(), edit_window])).grid(row=4, column=1)
 
 
-def save(data):
+def edit_slider(_id, w):
+    edit_window = Toplevel(w)
+    edit_window.title("Edytowanie slidera")
+    edit_window.geometry("600x400")
+
+    label = Label(edit_window, text="Edycja danych slidera").grid(row=0, column=1)
+    records = select_from_db(f"Select * from slides where id = '{_id + 1}'")
+    data = records[0]
+
+    tab = ["Tytuł", "Opis"]
+
+    e1 = Entry(edit_window, width=70)
+    e1.insert(END, (data[4]))
+    e1.grid(row=1, column=1, sticky=NSEW)
+
+    e2 = Entry(edit_window, width=70)
+    e2.insert(END, (data[5]))
+    e2.grid(row=2, column=1, sticky=NSEW)
+
+    for i in range(len(tab)):
+        l = Label(edit_window, text=tab[i]).grid(row=i + 1, column=0)
+
+    btn = Button(edit_window, text="Zapisz zmiany",
+                 command=lambda: save_slider([_id + 1, e1.get(), e2.get(), edit_window])).grid(row=4, column=1)
+
+
+def edit_menu(_id, w):
+    edit_window = Toplevel(w)
+    edit_window.title("Edytowanie menu")
+    edit_window.geometry("600x400")
+
+    label = Label(edit_window, text="Edycja danych menu").grid(row=0, column=1)
+    records = select_from_db(f"Select * from nav_links where id = '{_id + 1}'")
+    data = records[0]
+
+    tab = ["Tekst", "Link", "Komponent"]
+
+    e1 = Entry(edit_window, width=70)
+    e1.insert(END, (data[2]))
+    e1.grid(row=1, column=1, sticky=NSEW)
+
+    e2 = Entry(edit_window, width=70)
+    e2.insert(END, (data[1]))
+    e2.grid(row=2, column=1, sticky=NSEW)
+
+    e3 = Entry(edit_window, width=70)
+    e3.insert(END, (data[3]))
+    e3.grid(row=3, column=1, sticky=NSEW)
+
+    for i in range(len(tab)):
+        l = Label(edit_window, text=tab[i]).grid(row=i + 1, column=0)
+
+    btn = Button(edit_window, text="Zapisz zmiany",
+                 command=lambda: save_menu([_id + 1, e1.get(), e2.get(), e3.get(), edit_window])).grid(row=4, column=1)
+
+
+def edit_news(_id, w):
+    edit_window = Toplevel(w)
+    edit_window.title("Edytowanie artukułów")
+    edit_window.geometry("600x400")
+
+    label = Label(edit_window, text="Edycja danych artykułów").grid(row=0, column=1)
+    records = select_from_db(f"Select * from articles where id = '{_id + 1}'")
+    data = records[0]
+
+    tab = ["Tytuł", "Podtytuł", "Treść"]
+
+    e1 = Entry(edit_window, width=70)
+    e1.insert(END, (data[1]))
+    e1.grid(row=1, column=1, sticky=NSEW)
+
+    e2 = Entry(edit_window, width=70)
+    e2.insert(END, (data[2]))
+    e2.grid(row=2, column=1, sticky=NSEW)
+
+    e3 = Entry(edit_window, width=70)
+    e3.insert(END, (data[3]))
+    e3.grid(row=3, column=1, sticky=NSEW)
+
+    for i in range(len(tab)):
+        l = Label(edit_window, text=tab[i]).grid(row=i + 1, column=0)
+
+    btn = Button(edit_window, text="Zapisz zmiany",
+                 command=lambda: save_news([_id + 1, e1.get(), e2.get(), e3.get(), edit_window])).grid(row=4, column=1)
+
+
+def save_users(data):
     print(data)
+    data[4].destroy()
     connection = sqlite3.connect('db.sqlite')
     cursor = connection.cursor()
 
-    cursor.execute(f"UPDATE users SET username = '{data[1]}', password = '{data[2]}', role = '{data[3]}' WHERE id = {data[0]};")
+    cursor.execute(
+        f"UPDATE users SET username = '{data[1]}', password = '{data[2]}', role = '{data[3]}' WHERE id = {data[0]};")
+    connection.commit()
+
+
+def save_slider(data):
+    data[3].destroy()
+    connection = sqlite3.connect('db.sqlite')
+    cursor = connection.cursor()
+
+    cursor.execute(
+        f"UPDATE slides SET title = '{data[1]}', subtitle = '{data[2]}' WHERE id = {data[0]};")
+    connection.commit()
+
+
+def save_menu(data):
+    data[4].destroy()
+    connection = sqlite3.connect('db.sqlite')
+    cursor = connection.cursor()
+
+    cursor.execute(
+        f"UPDATE nav_links SET text = '{data[1]}', link = '{data[2]}', for_component = '{data[3]}' WHERE id = {data[0]};")
+    connection.commit()
+
+
+def save_link(data):
+    data[4].destroy()
+    connection = sqlite3.connect('db.sqlite')
+    cursor = connection.cursor()
+
+    cursor.execute(
+        f"INSERT INTO nav_links (link, text, for_component, order) VALUES ({data[0], data[1], data[2], data[3]});")
+    connection.commit()
+
+
+def save_news(data):
+    data[4].destroy()
+    connection = sqlite3.connect('db.sqlite')
+    cursor = connection.cursor()
+
+    cursor.execute(
+        f"UPDATE articles SET title = '{data[1]}', subtitle = '{data[2]}', content = '{data[3]}' WHERE id = {data[0]};")
     connection.commit()
 
 
